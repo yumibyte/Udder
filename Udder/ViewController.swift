d2540cee90a79773765662f8a65200044f300c68
Yumibyte <ashraigosa@gmail.com>
2019-07-07 17:24:16 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ashley Raigosa on 7/7/19.
+//  Copyright © 2019 Ashley Raigosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
